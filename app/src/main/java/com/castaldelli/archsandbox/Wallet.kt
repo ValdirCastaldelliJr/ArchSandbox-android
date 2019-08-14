e902564cb48c31e76a4cbf3553107a535da0e635
Valdir Castaldelli Junior <valdir.castaldelli@gmail.com>
2019-08-14 17:41:10 -0300

    Essas nao eram pra ter subido ...

--- app/src/main/java/com/castaldelli/archsandbox/Wallet.kt
+++
@@ -1,7 +0,0 @@
-package com.castaldelli.archsandbox
-
-interface Wallet {
-    fun getValue(): Int // initial value should be 0
-    fun add(valueToAdd: Int)
-    fun subtract(valueToSubtract: Int)
-}
